ef238ebd4064769c4329c9bbfb9ebf4a376e2129
zhanghua <582605786@qq.com>
2017-11-14 18:56:37 +0800

    Initial commit

---
+++ sample/src/main/java/com/nostra13/universalimageloader/sample/activity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.nostra13.universalimageloader.sample.activity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.sample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView=findViewById(R.id.img);
+        DisplayImageOptions options=new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .build();
+
+        ImageLoader.getInstance().displayImage("",imageView,options);
+    }
+}
